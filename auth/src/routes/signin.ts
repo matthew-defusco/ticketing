78c1c2e27f39e6128b92dfa672a43ea324490f1a
Matthew DeFusco <matthewdefusco@Matthews-MacBook-Pro.local>
2022-06-15 15:59:10 -0400

    first commit

---
+++ auth/src/routes/signin.ts
@@ -0,0 +1,53 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { User } from '../models/user';
+import { Password } from '../services/password';
+import { validateRequest } from '../middlewares/validation-request';
+import { BadRequestError } from '../errors/bad-request-error';
+
+const router = express.Router();
+
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid.'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password.'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Login request failed.');
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordsMatch) {
+      throw new BadRequestError('Login request failed.');
+    }
+
+    // Generate JWT
+
+    const existingUserJwt = jwt.sign(
+      { id: existingUser.id, email: existingUser.email },
+      process.env.JWT_KEY!
+    );
+
+    // STORE it on the Session Object inside a cookie
+    req.session = { jwt: existingUserJwt };
+
+    res.status(200).send(existingUser);
+  }
+);
+
+export { router as signInRouter };
---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,50 @@
+import express, { Request, Response, NextFunction } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { User } from '../models/user';
+import { validateRequest } from '../middlewares/validation-request';
+import { BadRequestError } from '../errors/bad-request-error';
+
+const router = express.Router();
+
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid.'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      // console.log('Email in use');
+      // return res.send({});
+      throw new BadRequestError('Email in use');
+    }
+
+    const user = User.build({ email, password });
+
+    await user.save();
+
+    // Generate JWT
+
+    const userJwt = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_KEY!
+    );
+
+    // STORE it on the Session Object inside a cookie
+    req.session = { jwt: userJwt };
+
+    res.status(201).send(user);
+  }
+);
+
+export { router as signUpRouter };
